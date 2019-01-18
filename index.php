b8137cbf6726a4310bc4cc9a7965038039c13417
Alex Inkin <ANInkin@mail.ru>
2019-01-18 15:25:54 +0300

    fifth commit

--- index.php
+++ index.php
@@ -15,6 +15,24 @@
 	<title>Сервис сбора вебметрических данных</title>
 </head>
 <body>
-    
+    <div class="content">
+    	<div class="box">
+    		<div class="box-title">
+    			<h1>Анализ сайта</h1>
+    		</div>
+    		<div class="box-content">
+    			<p>
+    				Если вы всерьез заинтересованы в эффективности оптимизации своего ресурса, то вам придется постоянно проводить мониторинг состояния сайта. Только в этом случае вы сможете не просто исправить существующие ошибки, но и предотвратить появление новых ошибок в дальнейшем. Анализ сайтов предоставляет уникальную возможность получить исчерпывающую информацию относительно вашего домена и сайта.
+    			</p>
+    			<hr>
+    			<form method="post" onsubmit="get_action(this);" class="form-inline form-box">
+    				<button type="submit" class="btn btn-primary pull-right">Проверить</button>
+    				<div style="overflow:hidden;zoom:1;">
+    					<input name="url" type="text" class="form-control" autofocus="autofocus" style="width:96%;" placeholder="Введите сайт...">
+    				</div>
+    			</form>
+    		</div>
+    	</div>
+    </div>
 </body>
 </html>
